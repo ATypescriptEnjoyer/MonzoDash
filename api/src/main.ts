154020e1640d349469616fd83ee1b134c43aca82
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2022-10-13 20:28:52 +0100

    Setting up everything for deployment

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('/api');
   await app.listen(5000);
 }
 bootstrap();
