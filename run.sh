c974ee94d5a614a7c5b275db532eaa5d88053e1c
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2022-10-14 16:02:05 +0100

    Added stop command to run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ echo "Running CMD $2 on $1 environment."
 if [[ $2 = 'start' ]] 
 then
     docker compose --env-file .env -f docker-compose.$1.yml up -d --build
+elif [[ $2 = 'stop' ]] 
+then
+    docker compose --env-file .env -f docker-compose.$1.yml down
 elif [[ $2 = 'logs' ]]
 then
     docker compose -f docker-compose.$1.yml logs -f
