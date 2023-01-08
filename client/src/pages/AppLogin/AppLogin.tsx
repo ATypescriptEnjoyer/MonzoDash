6fab64adb0b239bd30659f8395a7c6bd8cca1d45
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2023-01-08 23:38:46 +0000

    Fixed issues with the AppLogin page

--- client/src/pages/AppLogin/AppLogin.tsx
+++ client/src/pages/AppLogin/AppLogin.tsx
@@ -12,10 +12,27 @@ import { Button } from '@mui/material';
 
 export const AppLogin = (): JSX.Element => {
   const [authCodeSent, setAuthCodeSent] = useState(false);
-  const [authCode, setAuthCode] = useState<{ [indx: number]: number }>({});
+  const [authCode, setAuthCode] = useState<{ [indx: number]: number | string }>({});
   const refs = useRef<HTMLElement[]>([]);
 
   useEffect(() => {
+    const defaultValues = Array.from(new Array(6).keys()).reduce((prev, _, indx) => {
+      prev = { ...prev, [indx]: '' };
+      return prev;
+    }, {});
+    setAuthCode(defaultValues);
+  }, []);
+
+  useEffect(() => {
+    const validateCode = async (): Promise<void> => {
+      const connectedCode = Object.values(authCode).join('');
+      const { data } = await ApiConnector.post<boolean>('/login/auth-code', { code: connectedCode });
+      if (data) {
+        localStorage.setItem('auth-code', connectedCode);
+        window.location.reload();
+      }
+    };
+
     if (Object.values(authCode).join('').length === 6) {
       validateCode();
     }
@@ -26,22 +43,8 @@ export const AppLogin = (): JSX.Element => {
     setAuthCodeSent(true);
   };
 
-  const validateCode = async (): Promise<void> => {
-    const connectedCode = Object.values(authCode).join('');
-    const { data } = await ApiConnector.post<boolean>('/login/auth-code', { code: connectedCode });
-    if (data) {
-      localStorage.setItem('auth-code', connectedCode);
-      window.location.reload();
-    }
-  };
-
-  const onDigitChange = (index: number, digit: string): void => {
-    digit = digit.substring(digit.length - 1);
-    const digitNum = parseInt(digit);
-    if (Number.isNaN(digitNum)) {
-      return;
-    }
-    setAuthCode((code) => ({ ...code, [index]: digitNum }));
+  const onDigitChange = (index: number, digit: number): void => {
+    setAuthCode((code) => ({ ...code, [index]: digit }));
     if (index < 5) {
       refs.current[index + 1].focus();
     }
@@ -56,9 +59,11 @@ export const AppLogin = (): JSX.Element => {
           <StyledAuthCodeContainer>
             {Array.from(new Array(6).keys()).map((_, indx) => (
               <StyledAuthCodeDigit
+                key={indx}
+                type="number"
                 value={authCode[indx]}
                 ref={(el): HTMLElement => (refs.current[indx] = el as HTMLElement)}
-                onChange={(event): void => onDigitChange(indx, event.currentTarget.value)}
+                onChange={(event): void => onDigitChange(indx, event.currentTarget.valueAsNumber)}
               />
             ))}
           </StyledAuthCodeContainer>
