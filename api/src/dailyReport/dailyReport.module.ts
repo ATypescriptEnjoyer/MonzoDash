7d459b44c660f49ff0ebf04b0fe37db7f7d004f9
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2023-09-30 18:03:04 +0100

    Fixed broken module

--- api/src/dailyReport/dailyReport.module.ts
+++ api/src/dailyReport/dailyReport.module.ts
@@ -3,12 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DailyReportController } from './dailyReport.controller';
 import { DailyReportService } from './dailyReport.service';
 import { DailyReport, DailyReportSchema } from './schemas/dailyReport.schema';
-import { TransactionsModule } from 'src/transactions/transactions.module';
+import { MonzoModule } from 'src/monzo/monzo.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: DailyReport.name, schema: DailyReportSchema }]),
-    forwardRef(() => TransactionsModule),
+    forwardRef(() => MonzoModule),
   ],
   controllers: [DailyReportController],
   providers: [DailyReportService],
