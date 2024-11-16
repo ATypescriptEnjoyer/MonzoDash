13ebcf937db9207f0333ceb00729b747b5cccbea
Sasha <8694395+ATypescriptEnjoyer@users.noreply.github.com>
2024-11-16 13:19:40 +0000

    Clean up webhook

--- api/src/monzo/monzo.controller.ts
+++ api/src/monzo/monzo.controller.ts
@@ -13,6 +13,7 @@ import { WebhookTransaction } from './monzo.interfaces';
 import { MonzoService, Pot } from './monzo.service';
 import { Transactions } from '../transactions/schemas/transactions.schema';
 import { PotPaymentsService } from 'src/potPayments/potPayments.service';
+import { getTransactionDescription } from 'src/util/getTransactionDescription';
 
 @Controller('monzo')
 export class MonzoController {
@@ -39,73 +40,46 @@ export class MonzoController {
   @Post('webhook')
   async webhook(@Body() transaction: WebhookTransaction): Promise<void> {
     try {
-      if (transaction.type === 'transaction.created') {
-        if (transaction.data.amount === 0) {
-          return;
-        }
-        let description = transaction.data.merchant?.name || transaction.data.counterparty?.name;
-        const type = transaction.data.amount > 0 ? 'incoming' : 'outgoing';
-        if (!description && transaction.data.description.startsWith('pot_')) {
-          const pots = await this.monzoService.getPots();
-          const pot = pots.find((pot) => pot.id === transaction.data.description);
-          if (pot) {
-            description = `${type === 'incoming' ? 'Withdrawal from' : 'Deposit to'} ${pot.name}`;
-          } else {
-            description = type === 'incoming' ? 'Withdrawal from pot' : 'Deposit to pot';
-          }
-        }
-        if (description === 'Flex') {
-          description = transaction.data.notes;
-        }
-        const finances = await this.financesService.getAll();
-        const salary = finances.find((finance) => finance.id === '0').amount ?? 99999;
-        const amount = Math.abs(transaction.data.amount) / 100;
-        const isSalaryPayment = amount >= salary - salary / 20;
-        const employer = (await this.employerService.getAll())[0];
-        const employerName = employer ? employer.name : '';
-        const isPaymentFromEmployer = employerName === '' ? isSalaryPayment : employerName === description; //Allow empty employer name for cases of unsure employer bank name.
-        if (employerName === '' && isSalaryPayment) {
-          employer.name = description;
-          await this.employerService.save(employer);
-        }
-        await this.transactionService.save({
-          id: transaction.data.id,
-          amount,
-          created: transaction.data.created,
-          type,
-          logoUrl: transaction.data.merchant?.logo,
-          description: description.trim(),
-          transaction: transaction.data,
-          groupId: transaction.data.merchant.group_id,
-        } as Transactions);
-        if (transaction.ignoreProcessing) {
-          return;
-        }
-        if (isPaymentFromEmployer) {
-          if (isSalaryPayment) {
-            async.eachSeries(finances, async (potInfo: Finances) => {
-              if (potInfo.id !== '0' && potInfo.amount > 0) {
-                await this.monzoService.depositToPot(
-                  potInfo.id,
-                  Math.trunc(potInfo.amount * 100),
-                  transaction.data.account_id,
-                );
-              }
-            });
-          }
-        } else {
-          const potPayments = await this.potPaymentsService.getAll();
-          const possiblePayPot = potPayments.find(
-            (potPayment) => potPayment.groupId === transaction.data.merchant.group_id,
-          );
-          if (possiblePayPot) {
-            await this.monzoService.withdrawFromPot(
-              possiblePayPot.potId,
-              transaction.data.amount,
-              transaction.data.account_id,
-            );
-          }
-        }
+      if (transaction.type !== 'transaction.created' || transaction.data.amount === 0) {
+        return;
+      }
+      const description = getTransactionDescription(transaction.data, await this.monzoService.getPots());
+      const finances = await this.financesService.getAll();
+      const salary = finances.find((finance) => finance.id === '0').amount ?? 99999;
+      const amount = Math.abs(transaction.data.amount) / 100;
+      const isSalaryPayment = amount >= salary - salary / 20;
+      const employer = (await this.employerService.getAll())[0];
+      const employerName = employer ? employer.name : '';
+      const isPaymentFromEmployer = employerName === '' ? isSalaryPayment : employerName === description; //Allow empty employer name for cases of unsure employer bank name.
+      if (employerName === '' && isSalaryPayment) {
+        employer.name = description;
+        await this.employerService.save(employer);
+      }
+      await this.transactionService.save({
+        id: transaction.data.id,
+        amount,
+        created: transaction.data.created,
+        type: amount > 0 ? 'incoming' : 'outgoing',
+        logoUrl: transaction.data.merchant?.logo,
+        description: description.trim(),
+        transaction: transaction.data,
+        groupId: transaction.data.merchant?.group_id,
+      } as Transactions);
+      if (transaction.ignoreProcessing) {
+        return;
+      }
+      if (isPaymentFromEmployer && isSalaryPayment) {
+        async.eachSeries(
+          finances.filter((finance) => finance.id !== '0' && finance.amount > 0),
+          (potInfo: Finances) =>
+            this.monzoService.depositToPot(potInfo.id, Math.trunc(potInfo.amount * 100), transaction.data.account_id),
+        );
+        return;
+      }
+      const potPayments = await this.potPaymentsService.getAll();
+      const payPot = potPayments.find((potPayment) => potPayment.groupId === transaction.data.merchant.group_id);
+      if (payPot) {
+        await this.monzoService.withdrawFromPot(payPot.potId, transaction.data.amount, transaction.data.account_id);
       }
     } catch (error) {
       //Writes the error, but prevents Monzo from calling over and over
---
+++ api/src/util/getTransactionDescription.ts
@@ -0,0 +1,17 @@
+import { WebhookTransactionData } from 'src/monzo/monzo.interfaces';
+import { Pot } from 'src/monzo/monzo.service';
+
+export const getTransactionDescription = (transaction: WebhookTransactionData, pots: Pot[]): string => {
+  let description = transaction.merchant?.name || transaction.counterparty?.name;
+  if (description === 'Flex') {
+    return transaction.notes;
+  }
+  if (description) {
+    return description;
+  }
+  if (transaction.description.startsWith('pot_')) {
+    const potName = pots.find((pot) => pot.id === transaction.description)?.name || 'pot';
+    return `${transaction.amount > 0 ? 'Withdrawal from' : 'Deposit to'} ${potName}`;
+  }
+  return 'Unknown Transaction';
+};
