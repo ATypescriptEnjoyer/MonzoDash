314035946d0d01dd97942df22e974fb6fa25ad9a
Sasha <8694395+ATypescriptEnjoyer@users.noreply.github.com>
2024-11-19 17:26:01 +0000

    Fix db location

--- libs/db/src/lib/db.ts
+++ libs/db/src/lib/db.ts
@@ -3,7 +3,7 @@ import { DataSourceOptions, DataSource } from 'typeorm';
 export function getConfig() {
   return {
     type: 'better-sqlite3',
-    database: 'monzodash.db',
+    database: '/data/monzodash.db',
     synchronize: false,
     migrations: [__dirname + '/../migrations/*.{ts,js}'],
   } as DataSourceOptions;
