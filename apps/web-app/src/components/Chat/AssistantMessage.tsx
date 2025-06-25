6df4f08d88b9a04d02eaf138e73cd4e97bc92d3b
Puppy <8694395+ATypescriptEnjoyer@users.noreply.github.com>
2025-06-25 13:36:03 +0100

    darken colours

--- apps/web-app/src/components/Chat/AssistantMessage.tsx
+++ apps/web-app/src/components/Chat/AssistantMessage.tsx
@@ -1,9 +1,9 @@
 import { colours } from '../../theme';
-import { Stack, Typography } from "@mui/material";
+import { darken, Stack, Typography } from "@mui/material";
 import SyncLoader from 'react-spinners/SyncLoader';
 
 export const AssistantMessage = ({ message, thinking }: { message: string, thinking: boolean }) => {
-  return <Stack padding={1} borderRadius={1} sx={{ backgroundColor: (theme) => theme.palette.secondary.main }} gap={1} width={thinking ? 'auto' : "75%"} alignSelf="flex-end">
+  return <Stack padding={1} borderRadius={1} sx={{ backgroundColor: (theme) => darken(theme.palette.secondary.main, 0.5) }} gap={1} width={thinking ? 'auto' : "75%"} alignSelf="flex-end">
     {thinking ? <SyncLoader cssOverride={{ display: 'block' }} color={colours.white} size={10} speedMultiplier={0.5} /> : <Typography>
       {message.split('**').map((part, index) =>
         index % 2 === 1 ? <Typography key={index} component="span" sx={{ fontWeight: 'bold' }}>{part}</Typography> : part
